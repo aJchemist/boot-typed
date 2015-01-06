5312f19d572bc6b7e65dbc0feee14df9e8456df1
Daniel Ziltener <zilti@users.noreply.github.com>
2015-01-06 04:25:02 +0100

    Create README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+boot-typed
+==========
+
+[![Clojars Project](http://clojars.org/zilti/boot-typed/latest-version.svg)](http://clojars.org/zilti/boot-typed)
+
+A boot task allowing you to check your project using `core.typed`.
+
+Usage:
+
+```
+Options:
+  -h, --help                           Print this help info.
+  -c, --check                          Type check all Clojure namespaces declared in 'namespace'.
+  -n, --namespace NAMESPACE            Conj NAMESPACE onto a list of Clojure namespaces to check.
+  -j, --check-cljs                     Type check all ClojureScript namespaces declared in 'cljs-namespace'.
+  -s, --cljs-namespace CLJS-NAMESPACE  Conj [CLJS NAMESPACE] onto a list of ClojureScript namespaces to check.
+  -v, --coverage                       basic type coverage for all namespaces declared in 'namespace'.
+  -y, --nsym NSYM                      Conj NSYM onto a list of nsyms to override the namespace/cljs-namespace lists in build.boot, and for coverage.
+```
+
+Details
+-------
+
+- The coverage check works only for Clojure files and thus uses the namespaces defined using --namespace.
+- NSYM overrides both NAMESPACE and CLJS-NAMESPACE for check, check-cljs and coverage.
+- You may chain checks together; `boot typed check check-cljs coverage` runs just fine.
---
+++ src/zilti/boot_typed.clj
@@ -0,0 +1,60 @@
+(ns zilti.boot-typed
+  {:boot/export-tasks true}
+  (require [boot.core :as core]
+           [boot.pod :as pod]
+           [clojure.java.io :as io]))
+
+(def pod-deps '[])
+
+(defn init [fresh-pod impl]
+  (doto fresh-pod
+    (pod/with-eval-in
+      (case ~impl
+        :clj (require 'clojure.core.typed)
+        :cljs (require 'cljs.core.typed)))))
+
+(defn Check [impl worker-pods namespace nsym]
+  (let [nsym (if (seq nsym) nsym namespace)]
+    (assert (seq nsym))
+    (pod/with-eval-in (worker-pods :refresh)
+      (case ~impl
+        :clj (require 'clojure.core.typed)
+        :cljs (require 'cljs.core.typed))
+      (let [res (try (~(case impl
+                         :clj `clojure.core.typed/check-ns
+                         :cljs `cljs.core.typed/check-ns*)
+                      '~nsym)
+                     (catch Exception e
+                       (println (.getMessage e))))]
+        (prn res)))))
+
+(defn Coverage [impl worker-pods namespace nsym]
+  (let [nsym (if (seq nsym) nsym namespace)]
+    (assert (seq nsym))
+    (pod/with-eval-in (worker-pods :refresh)
+      (case ~impl
+        :clj (require 'clojure.core.typed)
+        :cljs (require 'cljs.core.typed))
+      (clojure.core.typed/var-coverage '~nsym))))
+
+(core/deftask typed
+  "Run a core.typed check in boot."
+  [c check bool "Type check all Clojure namespaces declared in 'namespace'."
+   n namespace NAMESPACE #{sym} "a list of Clojure namespaces to check."
+   j check-cljs bool "Type check all ClojureScript namespaces declared in 'cljs-namespace'."
+   s cljs-namespace CLJS-NAMESPACE #{sym} "a list of ClojureScript namespaces to check."
+   v coverage bool "basic type coverage for all namespaces declared in 'namespace'."
+   y nsym NSYM #{sym} "a list of nsyms to override the namespace/cljs-namespace lists in build.boot, and for coverage."]
+  (let [worker-pods (pod/pod-pool (update-in (core/get-env) [:dependencies] into pod-deps))]
+    (core/cleanup (worker-pods :shutdown))
+    (core/with-pre-wrap fileset
+      (when check
+        (println "Running Clojure check...")
+        (Check :clj worker-pods namespace nsym))
+      (when check-cljs
+        (println "Running ClojureScript check...")
+        (Check :cljs worker-pods namespace nsym))
+      (when coverage
+        (println "Running type coverage...")
+        (Coverage :clj worker-pods namespace nsym))
+      (core/commit! fileset))))
